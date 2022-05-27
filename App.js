cebd75ef7a4f3e0b4dfb503140b036115171f4b9
Billy <billypatrick767@gmail.com>
2022-05-27 20:19:11 +0300

    added: implementation to use screens

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
-export default () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>Things Todo</Text>
-      <Text>Coming soon!</Text>
-    </SafeAreaView>
-  );
-};
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home} from './screens';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
