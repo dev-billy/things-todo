2658dfabbb18182b27bdbc6dd72a25a1f1ecfc4c
Billy <billypatrick767@gmail.com>
2022-05-28 18:14:35 +0300

    fix: adding, removing todo items in state

--- components/AddListForm.js
+++ components/AddListForm.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Input} from 'react-native-elements';
 
 import {Text, View, StyleSheet, FlatList} from 'react-native';
+import TodoItem from './TodoItem';
 
 export default ({close}) => {
+  const [todoItems, setTodoItems] = useState([]);
+  const [todoItemErr, setTodoItemErr] = useState('');
+  const [todoText, setTodoText] = useState('');
+
+  const onTextChange = text => {
+    setTodoText(text);
+    setTodoItemErr('');
+  };
+  const addTodo = () => {
+    if (todoText.trim() === '') {
+      setTodoItemErr('Todo text is required');
+      return;
+    }
+    setTodoItems(currentItems => [
+      {id: currentItems.length + 1, text: todoText},
+      ...currentItems,
+    ]);
+    //clears the current text
+    setTodoText('');
+  };
+
+  const removeItem = id => {
+    setTodoItems(currentItems => {
+      return currentItems.filter(item => item.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.listTitle}>
@@ -15,7 +43,7 @@ export default ({close}) => {
           style={styles.input}
         />
       </View>
-      <View style={styles.todoItems}>
+      <View>
         <Text style={styles.todoItemsText}>Todo Items</Text>
         <View style={styles.todoItemsInputGrp}>
           <Input
@@ -23,11 +51,25 @@ export default ({close}) => {
             label="Add Item"
             labelStyle={styles.inputLabel}
             style={styles.input}
+            value={todoText}
+            errorMessage={todoItemErr}
+            onChangeText={onTextChange}
+            onSubmitEditing={addTodo}
           />
-          <Button buttonStyle={styles.buttonAddTodo} title="Add item" />
+          <Button
+            buttonStyle={styles.buttonAddTodo}
+            onPress={addTodo}
+            title="Add item"
+          />
         </View>
       </View>
-      <FlatList />
+      <FlatList
+        data={todoItems}
+        style={styles.todoItems}
+        renderItem={item => (
+          <TodoItem item={item.item} handleDelete={removeItem} />
+        )}
+      />
       <View style={styles.buttonGroup}>
         <Button
           type="clear"
@@ -67,6 +109,9 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     marginLeft: 10,
     paddingVertical: 20,
+  },
+  todoItems: {
+    marginTop: 20,
   },
   buttonAddTodo: {
     flexShrink: 0.3,
